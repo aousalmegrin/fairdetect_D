834eb8a26b1d6e040985d1a663e6b41572daf7ba
aousalmegrin <aous.almegrin@student.ie.edu>
2022-07-19 16:29:59 +0300

    first commit of the package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='fairdetect_D',
+      version='0.1',  # Development release
+      description='Python Packaging Starting Kit - Simple Structure to creater your own package - for training purposes!',
+      url='https://github.com/aousalmegrin/fairdetect_D',
+      author='Group D members',
+      author_email='aous.almegrin@student.ie.edu',
+      license='MIT',
+          packages=['fairdetect_D'],
+      zip_safe=False)
